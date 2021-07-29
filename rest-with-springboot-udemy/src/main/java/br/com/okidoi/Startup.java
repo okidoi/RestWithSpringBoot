0b56c230b08d873c10bb9ec026a778e43853252f
okidoi <okidoi@gmail.com>
2021-07-28 23:05:28 -0300

    Alteração

--- rest-with-springboot-udemy/src/main/java/br/com/okidoi/Startup.java
+++ rest-with-springboot-udemy/src/main/java/br/com/okidoi/Startup.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Startup {
 
-	//OBSERVACAO: Acesse no browser
-	//http://localhost:8080/greeting?name=Marcio
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
 	}
